70cc826030361512372b06752e6c26e45ae074f9
vkvincent <vinush10@hotmail.co.uk>
2022-05-14 14:32:55 +0100

    updated for hosting as gh project

--- src/Slideshow.js
+++
@@ -1,9 +0,0 @@
-const Slideshow = () => {
-    return ( 
-        <nav className="slideshow">
-            <img src={require('./img/1.png')} width='100%' alt="cocktails"/>
-        </nav>
-     );
-}
-
-export default Slideshow;
